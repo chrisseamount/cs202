f2bfb3a36212fccfb132a6770ca82694507b85fa
arparker5 <andrew.parkerr10@gmail.com>
2017-04-08 12:29:37 -0800

    solution commit

--- pushblox/pushblox/main.cpp
+++ pushblox/pushblox/main.cpp
@@ -1,16 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	sf::CircleShape shape(50.f);
+	shape.setFillColor(sf::Color::Red);
 
 	while (window.isOpen())
 	{
+		bool clr = false;
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+
+			switch (event.type)
+			{
+				case sf::Event::KeyPressed:
+					if (event.key.code == sf::Keyboard::Escape)
+					{
+						clr = !clr;
+						//shape.setFillColor(sf::Color::Blue);
+					}
+					break;
+
+				case sf::Event::Closed:
+					window.close();
+			}
+
+			if (clr)
+			{
+				shape.setFillColor(sf::Color::Red);
+			}
+			else {
+				shape.setFillColor(sf::Color::Blue);
+			}
+
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
