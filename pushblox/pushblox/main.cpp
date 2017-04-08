c9926b87e8d058d52acad414a226eaa54c244807
chrisseamount <chris_seamount@hotmail.com>
2017-04-08 14:55:22 -0800

    adding changes

--- pushblox/pushblox/main.cpp
+++ pushblox/pushblox/main.cpp
@@ -1,27 +1,8 @@
-#include <SFML/Graphics.hpp>
+#include "game.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 800), "pushBlox");
-	sf::CircleShape shape(350.f);
-
-	//Platform position
-	shape.setFillColor(sf::Color::Green);
-	shape.setPosition(50.f, 50.f);
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-
+	Game game;
+	game.start();
 	return 0;
 }
