3e971aa52c5bf7748e37349e39d70de7fbe05124
arparker5 <andrew.parkerr10@gmail.com>
2017-04-13 13:36:29 -0800

    player commit

--- pushblox/pushblox/player.cpp
+++ pushblox/pushblox/player.cpp
@@ -8,7 +8,7 @@ Player::Player()
 	rect.setPosition(400, 200);
 	rect.setFillColor(sf::Color::Black);
 	
-	sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+	sprite.setTextureRect(sf::IntRect(32*3, 0, 32, 32));
 	sprite.scale(2.0f, 2.0f);
 }
 
@@ -23,26 +23,26 @@ void Player::input()
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
 		rect.move(0, -_movementSpeed);
-		sprite.setTextureRect(sf::IntRect(_counterWalking * 32, 32 * 3, 32, 32));
+		sprite.setTextureRect(sf::IntRect((_counterWalking * 32)+(32*3), 32 * 3, 32, 32));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 	{
 		rect.move(0, _movementSpeed);
-		sprite.setTextureRect(sf::IntRect(_counterWalking * 32, 0, 32, 32));
+		sprite.setTextureRect(sf::IntRect((_counterWalking * 32) + (32 * 3), 0, 32, 32));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
 		rect.move(-_movementSpeed, 0);
-		sprite.setTextureRect(sf::IntRect(_counterWalking * 32, 32 * 1, 32, 32));
+		sprite.setTextureRect(sf::IntRect((_counterWalking * 32) + (32 * 3), 32 * 1, 32, 32));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
 		rect.move(_movementSpeed, 0);
-		sprite.setTextureRect(sf::IntRect(_counterWalking * 32, 32 * 2, 32, 32));
+		sprite.setTextureRect(sf::IntRect((_counterWalking * 32) + (32 * 3), 32 * 2, 32, 32));
 	}
 
 	_counterWalking++;
-	if (_counterWalking == 2)
+	if (_counterWalking == 3)
 	{
 		_counterWalking = 0;
 	}
