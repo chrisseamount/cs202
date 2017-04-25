ce39b02210829d19f1e03a436afe9f5678480995
chrisseamount <chris_seamount@hotmail.com>
2017-04-25 11:24:34 -0800

    applied the patch from babbies first branch

--- pushblox/pushblox/display.h
+++ pushblox/pushblox/display.h
@@ -1,22 +1,22 @@
-#pragma once
-
-#include<SFML/Graphics.hpp>
-
-namespace Display
-{
-	void init();
-	void clear();
-	void display();
-	void draw(const sf::Drawable& drawable);
-
-	bool isOpen();
-	void checkWindowEvents();
-	
-	void setView(int centerX, int centerY, int sizeX, int sizeY);
-	void changeView(float w, float h);
-
-	void closeWindow();
-
-	const int HEIGHT = 720;
-	const int WIDTH = 1280;
+#pragma once
+
+#include<SFML/Graphics.hpp>
+
+namespace Display
+{
+	void init();
+	void clear();
+	void display();
+	void draw(const sf::Drawable& drawable);
+
+	bool isOpen();
+	void checkWindowEvents();
+	
+	void setView(int centerX, int centerY, int sizeX, int sizeY);
+	void changeView(float w, float h);
+
+	void closeWindow();
+
+	const int HEIGHT = 720;
+	const int WIDTH = 1280;
 }
